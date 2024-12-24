d37ffb768adafc92c75ccb060d3fa53a7eeaa941
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-12-24 23:56:18 +0700

    btvn_ss21_bai3

---
+++ ss21-bai3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	FILE *fptr;
+	fptr= fopen("bt01.txt","a");
+	char text[100];
+	printf("nhap vao mot chuoi ");
+	fgets(text,100,stdin);
+	fprintf(fptr,"%s",text);
+	fclose(fptr);
+	return 0;
+}
+
